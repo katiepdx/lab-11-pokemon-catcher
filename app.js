a59c96ab56813c5aca32b8e2443a889fa8b04cd7
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-20 19:56:25 -0700

    updates gameRounds to page

--- app.js
+++ app.js
@@ -2,36 +2,48 @@ import { pokemonArray } from './data/pokemonArray.js';
 import { getRandomPokemon } from './pokemonUtils.js';
 
 // import functions and grab DOM elements
+const totalRounds = document.getElementById('total-rounds');
 const pokemonImgTags = document.querySelectorAll('img');
 const pokemonRadioTags = document.querySelectorAll('input');
+const pokemonLabels = document.querySelectorAll('label');
+const pokemonSelectionContainer = document.querySelector('pokemon-selection-container');
 const nextButton = document.getElementById('next-button');
 //validation
-console.log(pokemonImgTags, pokemonRadioTags, nextButton);
+console.log(totalRounds, pokemonImgTags, pokemonRadioTags, pokemonSelectionContainer, nextButton, pokemonLabels);
 
 // initialize state
-// const pokemon = pokemonArray.slice(); //copy of pokemonArray 
-// let gameRounds = 0;
-// let pokemonTimesCaptured = 0;
-// let pokemonCaptured = []; // array of pokemon captured
-// let pokemonNotCaptured = []; //array of not captured pokemon
+const pokemon = pokemonArray.slice(); //copy of pokemonArray 
+let gameRounds = 0;
+let pokemonTimesCaptured = 0;
+let pokemonCaptured = []; // array of pokemon captured
+let pokemonNotCaptured = []; //array of not captured pokemon
 
 // set event listeners to update state and DOM
-
-nextButton.addEventListener('click', () => {
-    let randomPokemonImg1 = getRandomPokemon(pokemonArray);
-    let randomPokemonImg2 = getRandomPokemon(pokemonArray);
-    let randomPokemonImg3 = getRandomPokemon(pokemonArray);
+function startGame() {
+    const randomPokemonImg1 = getRandomPokemon(pokemon);
+    let randomPokemonImg2 = getRandomPokemon(pokemon);
+    let randomPokemonImg3 = getRandomPokemon(pokemon);
     pokemonImgTags[0].src = randomPokemonImg1.url_image;
     pokemonImgTags[1].src = randomPokemonImg2.url_image;
     pokemonImgTags[2].src = randomPokemonImg3.url_image;
-
+    
     while (randomPokemonImg1 === randomPokemonImg2 || randomPokemonImg2 === randomPokemonImg3 || randomPokemonImg1 === randomPokemonImg3) {
-        let randomPokemonImg1 = getRandomPokemon(pokemonArray);
-        let randomPokemonImg2 = getRandomPokemon(pokemonArray);
-        let randomPokemonImg3 = getRandomPokemon(pokemonArray);
+        const randomPokemonImg1 = getRandomPokemon(pokemon);
+        let randomPokemonImg2 = getRandomPokemon(pokemon);
+        let randomPokemonImg3 = getRandomPokemon(pokemon);
         pokemonImgTags[0].src = randomPokemonImg1.url_image;
         pokemonImgTags[1].src = randomPokemonImg2.url_image;
         pokemonImgTags[2].src = randomPokemonImg3.url_image;
     }
+}
+
+startGame();
+
+nextButton.addEventListener('click', () => {
+    //update game rounds 
+    gameRounds = gameRounds + 1;
+    console.log(gameRounds);
+    totalRounds.textContent = `Round: ${gameRounds + 1}`;
+    startGame();
 });
 
--- pokemonUtils.js
+++ pokemonUtils.js
@@ -5,3 +5,4 @@ export function getRandomPokemon(pokemonArray) {
     //return a random position in pokemonArray
     return pokemonArray[randomPokemonIndex];
 }
+
