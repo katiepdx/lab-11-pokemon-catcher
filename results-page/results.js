75bccad504f672ed9411a9600de448b864b6e8ad
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-21 20:59:24 -0700

    adds reset scores button option and play again option

--- results-page/results.js
+++ results-page/results.js
@@ -1,10 +1,11 @@
 const statsDisplay = document.getElementById('results');
-const playAgainButton = document.getElementById('play-again-button');
+// const playAgainButton = document.getElementById('play-again-button');
+const resetStatsButton = document.getElementById('reset-stats-button');
 
-//get raw Stats to display 
 const backToArrayStats = localStorage.getItem('POKEMON STATS');
 statsDisplay.textContent = backToArrayStats;
 
-playAgainButton.addEventListener('click', () => {
+resetStatsButton.addEventListener('click', () => {
     localStorage.removeItem('POKEMON STATS');
+    statsDisplay.textContent = 'Stats have been reset';
 });
